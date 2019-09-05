5452d9e15d868981351d08fd08b5c0f0388d2ff2
shadow <wuh131420@foxmail.com>
2019-09-05 17:59:37 +0800

    2019-09-05-[feat][新增小组件][shadow]

---
+++ src/views/homePageManage/widget/egg/main.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class='my-page'>
+    <div class="b-egg">
+      <img class="b-egg__logo" :src="require('~~/index/bg_egg_small.png')" alt="egg">
+      <my-progress v-model="progress" style="position: relative; top: -.2rem; width: 1.5rem;"></my-progress>
+    </div>
+  </section>
+</template>
+
+<script>
+ import MyProgress from '@c/progress/main'
+ export default{
+  name: 'EggWidget',
+  components: {
+    MyProgress
+  },
+  props: {},
+  watch: {},
+  computed: {},
+  data(){
+    return {
+      progress: 0
+    }
+  },
+  methods: {},
+  created(){},
+  mixins: []
+ }
+</script>
+
+<style scoped lang='scss'>
+.my-page {
+  .b-egg {
+    &__logo {
+      position: relative;
+      bottom: 10%;
+      margin-left: 50%;
+      transform: translateX(-50%);
+      width: .8rem;
+      max-width: auto;
+    }
+  }
+}
+</style>
---
+++ src/views/homePageManage/widget/waitEgg/main.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class='my-page'>
+    <div class="b-wait">
+      <p class="b-wait__label">剩余产蛋时间: </p>
+      <my-progress v-model="progress" style="width: 2rem;">
+        <!-- <img class="b-progress__pre" slot="prefix" :src="require('~~/index/icon_egg_wait.png')" alt="logo"> -->
+        <span class="b-progress__pre" slot="prefix"></span>
+        <span class="b-progress__text" slot="inner">10小时40分</span>
+      </my-progress>
+    </div>
+  </section>
+</template>
+
+<script>
+ import MyProgress from '@c/progress/main';
+ export default{
+  name: 'WaitEgg',
+  components: {
+    MyProgress
+  },
+  props: {},
+  watch: {},
+  computed: {},
+  data(){
+    return {
+      progress: 0
+    }
+  },
+  methods: {},
+  created(){},
+  mixins: []
+ }
+</script>
+
+<style scoped lang='scss'>
+.my-page {
+  min-width: 2rem;
+  min-height: 2rem;
+  .b-wait {
+    width: inherit;
+    height: inherit;
+
+    &__label {
+      font-size: .28rem;
+      color: #4D3926;
+    }
+    .b-progress__pre {
+      width: 1rem;
+      height: 1rem;
+      border-radius: 50%;
+      // background-image: url('../../../assets/images/index/icon_egg_wait.png');
+    }
+    .b-progress__text {
+      color: #fff;
+      font-size: .28rem;
+      text-align: center;
+    }
+  }
+}
+</style>
