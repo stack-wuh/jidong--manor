758eb54971dc6e7ad657316f57a94d674cf06e1e
shadow <wuh131420@foxmail.com>
2019-09-09 15:44:46 +0800

    2019-09-09-[add][新增样式文件][shadow]

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,7 @@ import '@/assets/styles/reset.scss'
 import '@/assets/styles/base.scss'
 import './refreshRem.js'
 import '@a/styles/iconfont/iconfont.css'
+import '@a/styles/var.scss'
 
 import Vue from 'vue'
 
