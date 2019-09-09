7e1bb6e235b7dccb64968d191939f4a652cc2424
shadow <wuh131420@foxmail.com>
2019-09-09 15:47:36 +0800

    2019-09-09-[update][修改进度条组件的左侧icon][shadow]

--- src/views/homePageManage/widget/waitEgg/main.vue
+++ src/views/homePageManage/widget/waitEgg/main.vue
@@ -2,9 +2,9 @@
   <section class='my-page'>
     <div class="b-wait">
       <p class="b-wait__label">剩余产蛋时间: </p>
-      <my-progress v-model="progress" style="width: 2rem;">
-        <!-- <img class="b-progress__pre" slot="prefix" :src="require('~~/index/icon_egg_wait.png')" alt="logo"> -->
-        <span class="b-progress__pre" slot="prefix"></span>
+      <my-progress v-model="progress" style="width: 2rem; position: relative; border-radius: 20px;">
+        <img class="b-progress__pre" slot="prefix" :src="require('~~/index/icon_egg_wait.png')" alt="logo">
+        <!-- <span class="b-progress__pre" slot="prefix"></span> -->
         <span class="b-progress__text" slot="inner">10小时40分</span>
       </my-progress>
     </div>
@@ -45,14 +45,19 @@
       color: #4D3926;
     }
     .b-progress__pre {
-      width: 1rem;
-      height: 1rem;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: .4rem;
+      height: .4rem;
       border-radius: 50%;
+      background-color: #fff;
       // background-image: url('../../../assets/images/index/icon_egg_wait.png');
     }
     .b-progress__text {
+      margin-left: .5rem;
       color: #fff;
-      font-size: .28rem;
+      font-size: .26rem;
       text-align: center;
     }
   }
